4e395d63885a59ace916342db4b0d3ffbdb0ba2e
MarcinWalczak1 <walczakmarcin73@gmail.com>
2023-02-21 10:24:42 +0100

    Added Data.jsx

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Data = () => {
+    return(
+        <div className="home__data">
+            <h1 className="home__title">Marcin Walczak</h1>
+            <h3 className="home__subtitle">Front-end Developer</h3>
+
+            <a href="#contact" className="button button--flex">Say Hello</a>
+        </div>
+    )
+}
+export default Data
