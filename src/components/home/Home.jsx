0e8504f2a1b41082d9a59327f3647a22759de044
MarcinWalczak1 <walczakmarcin73@gmail.com>
2023-02-21 10:22:51 +0100

    Structure main part of site

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import "./home.css";
+import Data from "./Data"
 
 const Home = () => {
     return(
-        <div></div>
+        <section className="home section" id="home">
+            <div className="home__container container grid">
+                <div className="home__content grid">
+                    <Data/>
+                    <div className="home__img"></div>
+                </div>
+            </div>
+        </section>
     )
 
 }
