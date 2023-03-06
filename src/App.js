761092054eb7108377b84de9bc409ed01e4d4a43
MarcinWalczak1 <walczakmarcin73@gmail.com>
2023-03-06 19:35:18 +0100

    changes in import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/header/Header';
-import './App.css';
+import './App.scss';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/Skills/Skills';
